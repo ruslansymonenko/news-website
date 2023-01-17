c57c217afe09e7fc0f45ac3cf33c463f18e5ed7c
Ruslan Symonenko <symonenkors@gmail.com>
2023-01-17 12:13:49 +0200

    made header and footer with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import './App.css';
+import {Header} from './layout/Header.jsx';
+import {Footer} from './layout/Footer.jsx'
 
 function App() {
   return (
     <div className="App">
-      
+      <React.Fragment>
+        <Header/>
+        <Footer/>
+      </React.Fragment>
     </div>
   );
 }
