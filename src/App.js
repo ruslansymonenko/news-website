1d0d90de812910c5efbd46e530abb67940c463b6
Ruslan Symonenko <symonenkors@gmail.com>
2023-01-17 12:23:28 +0200

    made main part of website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Header} from './layout/Header.jsx';
-import {Footer} from './layout/Footer.jsx'
+import {Footer} from './layout/Footer.jsx';
+import {Main} from './layout/Main.jsx';
 
 function App() {
   return (
     <div className="App">
       <React.Fragment>
         <Header/>
+        <Main/>
         <Footer/>
       </React.Fragment>
     </div>
---
+++ src/layout/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Main.css';
+
+class Main extends React.Component {
+  state = {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render () {
+    return <div className="main-page">
+
+    </div>
+  }
+}
+
+export {Main}
